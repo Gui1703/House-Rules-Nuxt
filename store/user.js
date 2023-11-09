3c5d66cd0b73c97077cf882e0c7b47616e7277a0
Guilherme Raposo <guiraposo19@gmail.com>
2023-11-09 11:37:18 -0300

    feat: implements action, getter, mutation and state from user

---
+++ store/user.js
@@ -0,0 +1,31 @@
+export const state = () => ({
+  information: {},
+  token: '',
+})
+
+export const mutations = {
+  login(state, data) {
+    console.log(state, data)
+    state.information = data
+    state.token =
+      'Bearer 40fe071962846075452a4f6123ae71697463cad20f51e237e2035b41af0513d8'
+  },
+  logout(state) {
+    state.information = {}
+    state.token = ''
+  },
+}
+
+export const getters = {
+  userIsLogged(state) {
+    return state.token
+  },
+}
+
+export const actions = {
+  loginUser(context, user) {
+    return this.$axios
+      .$post('/login_json', user)
+      .then((res) => context.commit('user/login', res))
+  },
+}
