81e75dc0dfe04146ac5052741db8454cc3aee7f4
Guilherme Raposo <guiraposo19@gmail.com>
2023-11-09 08:53:12 -0300

    feat: implements input component

---
+++ components/sas-input.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="mb-3">
+    <label :for="name">{{ label }}</label>
+    <input
+      class="form-control"
+      :id="name"
+      v-model="model"
+      :type="type"
+      :name="name"
+      :required="required"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: [String, Number],
+      default: undefined,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    name() {
+      return this.label.toLowerCase()
+    },
+    model: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
+      },
+    },
+  },
+}
+</script>
