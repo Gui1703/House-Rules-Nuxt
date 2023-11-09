37520c6f41f133e543514b40c92f67bba798b926
Guilherme Raposo <guiraposo19@gmail.com>
2023-11-09 11:34:18 -0300

    feat: implements layout default

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="default">
+    <navbar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.default {
+  height: 100vh;
+  font-family: 'Montserrat Alternates', sans-serif;
+}
+</style>
